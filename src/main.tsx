e2cba333ad38e774721f478b1642f79de5c76768
Mariano d'Almeida <mdalmeida@gmail.com>
2026-03-15 19:02:37 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import App from './app.tsx'
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>)
